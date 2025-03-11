93338457ceaafd7e9c215b7d388094d3e9ca11b9
Shantelle Awomoyi <shantelleawo@gmail.com>
2025-03-11 08:54:17 +0000

    task 3 complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,6 +14,17 @@ afterAll(()=>{
 return db.end()
 })
 
+describe("/api/notAPath", () => {
+  test("ANY 404: responds with error message when path is not found", () => {
+    return request(app)
+      .get("/api/treasure")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("path not found");
+      });
+  });
+});
+
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
@@ -23,4 +34,65 @@ describe("GET /api", () => {
         expect(endpoints).toEqual(endpointsJson);
       });
   });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of correctly formatted topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({body}) => {
+      expect(body.topics.length).toBe(3);
+      body.topics.forEach((topic)=>{
+        expect(topic).toHaveProperty("slug")
+        expect(topic).toHaveProperty("description")
+      })
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: Responds with an object containing information of the article that has the requested id", () => {
+    return request(app)
+      .get(`/api/articles/1`)
+      .expect(200)
+      .then(({body}) => {
+      
+      const {author,
+        title,
+        article_id,
+        topic,
+
+        created_at,
+        votes,
+        article_img_url} = body.article;
+       
+        expect(title).toBe("Living in the shadow of a great man")
+        expect(topic).toBe("mitch")
+        expect(article_id).toEqual(1)
+        expect(author).toBe("butter_bridge")
+        expect(body.article.body).toBe("I find this existence challenging")
+        expect(created_at).toBe("2020-07-09T20:11:00.000Z")
+        expect(votes).toBe(100)
+        expect(article_img_url).toBe("https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700")
+      
+      });
+      
+  });
+  test("GET 400: responds with bad request", () => {
+    return request(app)
+      .get("/api/articles/notanumber")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("bad request");
+      });
+    })
+    test("GET 404: responds with 'not found' ", () => {
+      return request(app)
+        .get("/api/articles/900")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("not found");
+        });
+      })
 });
--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require("express")
 const app = express()
 const getAllApis = require("./controllers/api.controller")
+const { getAllTopics } = require("./controllers/topics.controllers");
+const { getArticleByArticleId } = require("./controllers/articles.controllers");
 
-app.get(`/api`, getAllApis)
+
+
+app.get(`/api`, getAllApis);
+app.get(`/api/topics`, getAllTopics);
+app.get(`/api/articles/:article_id`, getArticleByArticleId)
+
+
+
+app.all("*", (request, response) => {
+    response.status(404).send({ msg: "path not found" });
+  });
+
+  app.use((err, req, res, next) => {
+    if (err.code === "22P02") {
+      res.status(400).send({ msg: "bad request" });
+    }
+    next(err);
+  });
+
+  app.use((err, req, res, next) => {
+    if (err.code) {
+      res.status(err.code).send({ msg: err.msg });
+    }
+    next(err);
+  });
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ msg: "Internal Server Error" });
+  });
 
 module.exports = app
