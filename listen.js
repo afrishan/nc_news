92f8a64251ab7694ea1f455053a1e0a63b6f9ddd
Shantelle Awomoyi <shantelleawo@gmail.com>
2025-03-14 11:13:31 +0000

    13-setting up supabase

--- listen.js
+++ listen.js
@@ -1,5 +1,6 @@
 const app = require("./app")
+const { PORT = 3000 } = process.env;
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000...");
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
   });
